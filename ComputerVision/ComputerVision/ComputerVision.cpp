58bd49f7dcb9900f2edb5ffe5db98deba006a107
bas <bas.winkelhof@hotmail.nl>
2020-04-22 14:46:43 +0200

    default project

--- ComputerVision/ComputerVision/ComputerVision.cpp
+++ ComputerVision/ComputerVision/ComputerVision.cpp
@@ -1,6 +1,3 @@
-// Laden, omzetten naar grijswaarde beeld en opslaan van een beeld 
-// Uitleg: http://docs.opencv.org/doc/tutorials/introduction/load_save_image/load_save_image.html
-// Jan Oostindie, dd 22-1-2015
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -8,179 +5,29 @@
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
-#include <opencv2/opencv.hpp>
-#include "opencv2/imgproc/imgproc.hpp" 
-#include "opencv2/highgui/highgui.hpp"
-#include <iostream>
-#include <string>
-#include "blobdetectionavans.h"
-
-
 
 using namespace cv;
 using namespace std;
 
-
-/// Globale Variabelen voor de slider 
-const int area_slider_max = 20000;
-int default_area_sider = 4000;
-int areasizevalue = 100;
-
-Mat gray_image;
-Mat binaryx;
-
-
-// Callback functie voor afhandeling trackbar events
-void on_trackbar(int, void*)
+int main(int argc, char** argv)
 {
-	// waarde ophalen van de slider
-	areasizevalue = default_area_sider;
+	Mat image = imread("X:/settings/laptop background/boemboemboem.jpg");;
 
-	// met verkregen waarde een operatie uitvoeren
-	threshold(gray_image, binaryx, areasizevalue, 1, CV_THRESH_BINARY);
-
-	// nieuwe resultaat tonen
-	imshow("binair beeld", binaryx * 255);
-}
-
-
-int main()
-{
-
-
-	// Mat is een class voor objecten waarin een afbeelding kan worden opgeslagen.
-	Mat image = imread("../Images/ballen.jpg");
-
-	// Controleer of alles goed is gegaan
 	if (!image.data)
 	{
 		cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
 
-	// De afbeelding converteren naar een grijswaarde afbeelding
-	Mat gray_image;
-	cvtColor(image, gray_image, CV_BGR2GRAY);
+	// Laat de afbeelding zien in een apart window
+	namedWindow("Display window", WINDOW_AUTOSIZE);
+	imshow("Display window", image);
 
-	// Converteren naar grijswaarde afbeelding
-	cout << "Imagefile: met succes geconverteerd naar grijswaarde beeld." << endl;
-
-	//////////////////////////////////////////////////////////////////////////
-
-	// Grijswaarde afbeelding thresholden
-	Mat binaryImage;
-	int thresholdvalue = 200;
-	threshold(gray_image, binaryImage, thresholdvalue, 1, CV_THRESH_BINARY_INV);
-	namedWindow("Original", WINDOW_AUTOSIZE);
-	imshow("Original", gray_image);
+	// Wacht op een muiskklik in het window van de afbeelding
 	waitKey(0);
 
-	// Alvorens bewerkingen uit te voeren op het beeld converteren we deze
-	// naar een Mat object met grotere diepte (depth), t.w. 16 bits signed
-	Mat binary16S;
-	binaryImage.convertTo(binary16S, CV_16S);
-
-	// functie labelBLOBs doet hetzelfde als Label Blobs in VisionLab; input is een
-	// binair beeld. Output is een image waarin alle pixels van elke blob voorzien zijn van 
-	// volgnummer.
-	Mat labeledImage;
-	cout << "Total number of BLOBs " << labelBLOBs(binary16S, labeledImage) << endl;
-
-	// functie show16SImageStretch beeld elke image af op 0 - 255 zodat je vrijwel altijd
-	// wel een redelijke view krijgt op de data.
-	// (show16SImageClip laat alle pixels met een waarde tussen 0 en 255 zien. Waardes onder 0
-	// worden op 0 afgebeeld, waardes boven 255 worden op 255 afgebeeld.)
-	show16SImageStretch(labeledImage, "Labeled Image");
-
-	imwrite("c:\\dump\\labeledImage.jpg", labeledImage);
-
-	// labelBLOBsInfo: met deze functie kun je ook BLOBs labelen. De functie geeft van 
-	// alle BLOBs de volgende informatie terug:
-	// - coordinaten van het eerste pixel van de BLOB
-	// - coordinaten van het zwaartepunt van de BLOB.
-	// - area van de BLOB
-	Mat labeledImage2;
-	vector<Point2d*> firstpixelVec2;
-	vector<Point2d*> posVec2;
-	vector<int> areaVec2;
-	labelBLOBsInfo(binary16S, labeledImage2, firstpixelVec2, posVec2, areaVec2);
-	show16SImageStretch(labeledImage2, "Labeled Image 2");
-
-	cout << endl << "*******************************************" << endl << endl;
-
-	// Toon alle informatie in de console 
-	cout << "Aantal gevonden BLOBs = " << firstpixelVec2.size() << endl;
-	for (int i = 0; i < firstpixelVec2.size(); i++) 
-	{
-		cout << "BLOB " << i + 1 << endl;
-		cout << "firstpixel = (" << firstpixelVec2[i]->x << "," << firstpixelVec2[i]->y << ")" << endl;
-		cout << "centre = (" << posVec2[i]->x << "," << posVec2[i]->y << ")" << endl;
-		cout << "area = " << areaVec2[i] << endl;
-	}
-
-
-	// Rechtstreeks aanroepen van de callback functie om het eerste beeld te krijgen
-	//on_trackbar(areasizevalue, 0);
-
-
-	// Toon alle informatie in de console 
-	cout << "gevonden BLOBs met formaat kleiner dan 5000" << endl;
-	for (int i = 0; i < firstpixelVec2.size(); i++) 
-	{
-		if (areaVec2[i] < 5000) 
-		{
-			cout << "BLOB " << i + 1 << endl;
-			cout << "firstpixel = (" << firstpixelVec2[i]->x << "," << firstpixelVec2[i]->y << ")" << endl;
-			cout << "centre = (" << posVec2[i]->x << "," << posVec2[i]->y << ")" << endl;
-			cout << "area = " << areaVec2[i] << endl;
-		}
-	}
-
-
-	show16SImageStretch(labeledImage2, "Labeled Image 2");
-
-
-
-
-
-
-
-
-
-	cout << endl << "*******************************************" << endl << endl;
-
-	// Met de functie labelBLOBsInfo kun je ook een threshold instellen voor de oppervlakte
-	// van de BLOBs.
-
-	Mat labeledImage3;
-	vector<Point2d*> firstpixelVec3;
-	vector<Point2d*> posVec3;
-	vector<int> areaVec3;
-	int minArea = 15000;
-	int maxArea = 16000;
-	labelBLOBsInfo(binary16S, labeledImage3,
-		firstpixelVec3, posVec3, areaVec3, minArea, maxArea);
-	show16SImageStretch(labeledImage3, "Labeled Image 3");
-
-	cout << endl << "*******************************************" << endl << endl;
-
-	// Toon alle informatie in de console 
-	cout << "Aantal gevonden BLOBs = " << firstpixelVec3.size() << endl;
-	for (int i = 0; i < firstpixelVec3.size(); i++) {
-		cout << "BLOB " << i + 1 << endl;
-		cout << "firstpixel = (" << firstpixelVec3[i]->x << "," << firstpixelVec3[i]->y << ")" << endl;
-		cout << "centre = (" << posVec3[i]->x << "," << posVec3[i]->y << ")" << endl;
-		cout << "area = " << areaVec3[i] << endl;
-	}
-
-	cout << endl << "*******************************************" << endl << endl;
-
-	// Nog een manier om het aantal BLOBs te verkrijgen...
-	cout << "Total number of BLOBs = " << maxPixelImage(labeledImage3) << endl;
-
-	string pipo;
-	cin >> pipo;
+	// Ruim alle aangemaakte windows weer op.
+	destroyAllWindows();
 
 	return 0;
-
 }
