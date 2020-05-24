01a9c2fbafeccefebb6a70b36bd519ba17ff996e
bas <bas.winkelhof@hotmail.nl>
2020-05-24 22:06:19 +0200

    Visionapp 3 v1.0

---
+++ ComputerVision/ComputerVision/ColorDetection.h
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc/imgproc.hpp" 
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <string>
+
+class ColorDetection
+{
+public:
+	cv::Mat redColorDetection(cv::Mat frame);
+	void trackObject(cv::Mat frame, cv::Mat threshold);
+};
--- ComputerVision/ComputerVision/ComputerVision.cpp
+++ ComputerVision/ComputerVision/ComputerVision.cpp
@@ -9,14 +9,59 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 #include <string>
-#include "blobdetectionavans.h"
+#include "ColorDetection.h"
 
 using namespace cv;
 
 int main(int argc, char* argv[])
 {
-	std::cout << "vision" << std::endl;
-	std::cin.get();
+	std::cout << "Visionapp3 by Bas Winkelhof" << std::endl;
+
+	VideoCapture cap(1);
+
+	if (!cap.isOpened())
+	{
+		std::cout << "Failed to open video input" << std::endl;
+	}
+
+	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+	std::cout << "Frame size : " << dWidth << " x " << dHeight << std::endl;
+
+	namedWindow("Camera source", CV_WINDOW_AUTOSIZE);
+
+	Mat frame, redFrame;
+	ColorDetection detection;
+
+	while (true)
+	{
+		bool bSuccess = cap.read(frame);
+
+		if (!bSuccess)
+		{
+			std::cout << "Cannot read a frame from video stream" << std::endl;
+			break;
+		}
+
+		flip(frame, frame, 3);
+
+		imshow("Camera source", frame);
+
+		redFrame = detection.redColorDetection(frame);
+
+		detection.trackObject(frame, redFrame);
+		
+		namedWindow("Red objects", WINDOW_AUTOSIZE);
+		imshow("Red objects", frame);
+
+		if (cv::waitKey(1) == 27)
+		{
+			std::cout << "Esc key is pressed by user quitting application" << std::endl;
+			destroyAllWindows();
+			break;
+		}
+	}
 
 	return 0;
 }
+
